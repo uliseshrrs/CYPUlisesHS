515a2ff601e5c3875d7f96a46e62235dd1b58347
uliseshrrs <54962237+uliseshrrs@users.noreply.github.com>
2019-09-19 23:09:25 -0500

    Add files via upload

---
+++ Libro/EJEMPLO1.11.py
@@ -0,0 +1,9 @@
+#ULISES HERNANDEZ SANCHEZ
+A = int ( input ( " escribe un valor      A: " ))
+B = int ( input ( " escribe un valor      B: " ))
+C = int ( input ( " escribe un valor      C: " ))
+D = int ( input ( " escribe un valor      D: " ))
+print(f"{D}")
+print(f"{C}")
+print(f"{B}")
+print(f"{A}")
